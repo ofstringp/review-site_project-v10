b53bc99d3b230b50a50b45ac027d71a93418db31
edna <mo>
2021-02-07 19:45:21 -0500

    Initial Build +
    Configurations set

---
+++ layouts/LandingPageLayout.vue
@@ -0,0 +1,14 @@
+<template lang="">
+  <div>
+    <nuxt />
+    <nuxt-link to="/" class="is-primary">
+      <b-icon icon="alert" /> Home
+    </nuxt-link>
+  </div>
+</template>
+<script>
+export default {
+  name: 'LandingPageLayout'
+}
+</script>
+<style lang=""></style>
